adb690f4141977dc27747e19fe323caafa9d6351
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-02-23 07:12:55 +0330

    Regex&Validation-Part2

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -10,10 +10,29 @@ const RegisterForm = () => {
         },
         onSubmit: values => {
             console.log(values);            
+        },
+        validate: values => {
+          let errors = {}
+          if(!values.name){
+            errors.name = "لطفا این قسمت را پر کنید"
+          }
+          if(!values.email){
+            errors.email = "لطفا این قسمت را پر کنید"
+          }else if(!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(values.email)){
+            errors.email = "لطفا قالب ایمیل را درست وارد کنید: مثل www@gmail.com"
+          }
+          if(!values.password){
+            errors.password = "لطفا این قسمت را پر کنید"
+          }else if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(values.password)){
+            errors.password = "لطفا پسورد شامل حروف و عدد و علامت باشد"
+          }
+          return errors;
         }
     })
 
-    console.log(formik.values);
+    console.log(formik);
+    
+    // console.log(formik.values);
     
 
   return (
@@ -28,19 +47,35 @@ const RegisterForm = () => {
               <label htmlFor="name" className="block mb-1 text-gray-700 text-sm font-medium">نام</label>
               <input type="text" className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
               focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                id="name" name="name" value={formik.values.name} onChange={formik.handleChange}/>
+                id="name" name="name" value={formik.values.name} 
+                onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                {formik.errors.name  && formik.touched.name ? 
+                <small className="block text-center text-red-600">
+                  {formik.errors.name}
+                </small> : null}
             </div>
             <div className="mb-3">
                 <label htmlFor="email" className="block mb-1 text-gray-800 text-sm font-medium">ایمیل</label>
                 <input type="email" className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm 
                 focus:outline-none focus:ring-3 focus:ring-red-500 focus:border-red-600" 
-                id="email" name="email" value={formik.values.email} onChange={formik.handleChange}/>
+                id="email" name="email" value={formik.values.email} 
+                onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                {formik.errors.email && formik.touched.email ? 
+                <small className="block text-center text-red-600">
+                  {formik.errors.email}
+                </small> : null}
             </div>
             <div className="mb-3">
                 <label htmlFor="email" className="block mb-1 text-gray-900 text-sm font-medium">رمز عبور</label>
                 <input type="password" className="w-full px-3 py-2 border border-gray-400 rounded-md shadow-sm 
                 focus:outline-none focus:ring-3 focus:ring-green-500 focus:border-green-600" 
-                id="password" name="password" value={formik.values.password} onChange={formik.handleChange}/>
+                id="password" name="password" value={formik.values.password} 
+                onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                {formik.errors.password  && formik.touched.password ? 
+                <small className="block text-center text-red-600">
+                  {formik.errors.password}
+                </small> : null}
+
             </div>
             <div className="text-center">
                 <button className="p-2 bg-pink-600 rounded-sm text-white">ثبت نام</button>
